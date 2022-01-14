9da54057547edc5e63c284eff74c391cf9531140
Fabio Vaz <fabiovaz@gmail.com>
2022-01-14 10:59:42 -0300

    feat: index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,8 @@
 <template>
   <main>
-    <div class="landing">
-      <div class="image">
-        <img src="~assets/img/savethedate.jpg" />
-      </div>
-    </div>
+    <CardSchedule />
+    <CardSchedule />
+    <CardSchedule />
   </main>
 </template>
 
@@ -17,20 +15,5 @@ export default {
 <style lang="scss">
   body {
     font-family: 'Roboto', sans-serif;
-  }
-  .landing {
-      width: 100vw;
-      height: 100vh;
-      background: #f7f8f2;
-    .image {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      img {
-        max-width: 90vw;
-        max-height: 90vh;
-      }
-    }
   }
 </style>
